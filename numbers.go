e0cce7832762c3f7aebc371d39411c9cbcd92498
kanelian63 <kanelian63@naver.com>
2021-12-25 18:20:11 +0900

    numbers.go

---
+++ numbers.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c1 := 12 +1i
+	c2 := complex(5, 7)
+	fmt.Printf("type of c1: %T\n", c1)
+	fmt.Printf("type of c2: %T\n", c2)
+
+	var c3 complex64 = complex64(c1 + c2)
+	fmt.Printf("c3 :", c3)
+	fmt.Printf("type of c3: %T\n", c3)
+
+	cZero := c3 - c3
+	fmt.Printf("cZero :", cZero)
+
+	x := 12
+	k := 5
+	fmt.Println(x)
+	fmt.Printf("type of x: %T\n", x)
+
+	div := x / k
+	fmt.Printf("div:", div)
+
+	var m, n float64
+	m = 1.223
+	fmt.Println("m, n:", m, n)
+
+	y := 4 / 2.3
+	fmt.Println("y:", y)
+
+	divFloat := float64(x) / float64(k)
+	fmt.Println("divFloat", divFloat)
+	fmt.Printf("type of divFloat: %T\n", divFloat)
+}
