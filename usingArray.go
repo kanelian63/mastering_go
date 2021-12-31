90406dea57481b60733487a9a391c4ee5ad00045
kanelian63 <kanelian63@naver.com>
2022-01-01 00:16:29 +0900

    usingArray.go

---
+++ usingArray.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	anArray := [4]int{1,2,4,-4}
+	twoD := [4][4]int{{1,2,3,4}, {5,6,7,8},{9,10,1,2},{1,2,3,4}}
+	threeD := [2][2][2]int{{{1,2},{3,4}},{{5,6},{7,8}}}
+
+	fmt.Println("the length of", anArray, "is", len(anArray))
+	fmt.Println("the first element of", twoD, "is", twoD[0][0])
+	fmt.Println("the length of", threeD, "is", len(threeD))
+
+	for i := 0; i < len(threeD); i++ {
+		v := threeD[i]
+		for j := 0; i < len(v); j++ {
+			m := v[j]
+			for k := 0; k < len(m); k++ {
+				fmt.Print(m[k], " ")
+			}
+		}
+		fmt.Println()
+	}
+	for _, v := range threeD {
+		for _, m := range v {
+			for _, s := range m {
+				fmt.Print(s, " ")
+			}
+		}
+		fmt.Println()
+	}
+}
